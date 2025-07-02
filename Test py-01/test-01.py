a6f5c3d75b83954de514f7d001516b913d0f47ca
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2025-07-02 17:24:30 -0300

    first commit

---
+++ Test py-01/test-01.py
@@ -0,0 +1,7 @@
+import pyautogui #type: ignore
+
+pyautogui.write("#Primera emulacion") #Le indicamos a Python que escriba "#Primera emulación" como comentario
+pyautogui.press('enter') # El robot presiona 'enter' y muestra dónde el comentario. -> Ejecutar
+
+#Primera emulacion
+#Primera emulacion
